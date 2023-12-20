38465fb7ffd9c3e1ec55998bd106b55d954e350d
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-12-19 23:52:38 -0500

    minimally fixed errors

--- cli/ReceiptPage.java
+++ cli/ReceiptPage.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class ReceiptPage extends PageView {
 
     public ReceiptPage(Scanner input) {
+        super(input);
+    }
+
+    @Override
+    public boolean manageChoice(int choice) {
+        // TODO écrire la méthode
+        return true;
     }
     
 }
