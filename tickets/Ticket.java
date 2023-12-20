e09a9ef6f5fa8dd2cf1849293bfd04b7c35a6c29
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-12-19 22:16:40 -0500

    implemented Ticket

--- tickets/Ticket.java
+++ tickets/Ticket.java
@@ -1,5 +1,35 @@
 package tickets;
 
 public class Ticket {
+
+    private Show show;
+    private String owner;
+
+    public Ticket(Show show, String owner) {
+        this.show = show;
+        this.owner = owner;
+    }
+
+    public Show getShow() {
+        return show;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String toString() {
+        String printableTicket = "";
+        int width = 58;
+        int remaining = width - show.getName().length();
+        String fullLine = new String(new char[width+2]).replace("\0", "-");
+        String halfLine = new String(new char[remaining/2]).replace("\0", "-");
+        printableTicket += halfLine + " " + show.getName() + " " + halfLine + "\n\n";
+        printableTicket += "  Ce billet appartient à :\n";
+        printableTicket += "  " + owner + "\n\n";
+        printableTicket += fullLine + "\n";
+
+        return printableTicket;
+    }
     
 }
