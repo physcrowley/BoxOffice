28849caf72bd9edcb2e027ff69dba8bdc11b66e0
physcrowley <53307335+physcrowley@users.noreply.github.com>
2023-12-20 01:20:11 -0500

    added todos to ReceiptPage

--- cli/ReceiptPage.java
+++ cli/ReceiptPage.java
@@ -1,11 +1,18 @@
 package cli;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import tickets.Ticket;
 
 public class ReceiptPage extends PageView {
 
-    public ReceiptPage(Scanner input) {
+    // TODO ajouter une liste des Tickets et ajouter une initialisation appropriée dans le constructeur
+
+    /** La valeur de {@code tickets} devrait venir du {@link ShoppingPage} */
+    public ReceiptPage(Scanner input, ArrayList<Ticket> tickets) {
         super(input);
+        // TODO écrire le reste du constructeur
     }
 
     @Override
